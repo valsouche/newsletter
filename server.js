7e55a6339dafed1a12d5108c3c57bae7495028c1
Valentin <valentin.souche@gmail.com>
2015-12-07 11:29:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@
 
     mongoose.connect('mongodb://localhost/firemail');     // connect to mongoDB database on modulus.io
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
