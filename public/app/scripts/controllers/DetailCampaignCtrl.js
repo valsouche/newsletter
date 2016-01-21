adf3a5db3586ee7d30a093a3fac89384fad4fec0
emilie-jin <emilie.jin@hotmail.com>
2016-01-21 12:02:50 +0100

    ajouter alert dans page campaign
    ajouter css et alert dans page campaign

--- public/app/scripts/controllers/DetailCampaignCtrl.js
+++ public/app/scripts/controllers/DetailCampaignCtrl.js
@@ -8,11 +8,11 @@
  */
 
 
- 
+
 
   angular.module('newsletterApp')
-    .controller('DetailCampaignCtrl',['$scope','$routeParams','$http','$location',
-        function ($scope,$routeParams, $http, $location) {
+    .controller('DetailCampaignCtrl',['$scope','$routeParams','$http','$location','SweetAlert',
+        function ($scope,$routeParams, $http, $location,SweetAlert) {
           $scope.detailCampaign = {};
           var id = $routeParams.campaign_id;
           $http.get('/api/campaignsDetail/' + id)
@@ -22,20 +22,38 @@
             .error(function(data) {
               console.log('Error: ' + data);
             });
-        
 
-       
+
+
          $scope.updateCampaign = function(){
-            $http.put('/api/updateCampaigns/' + $routeParams.campaign_id, $scope.detailCampaign)
-              .success(function(data) {
-                $scope.detailCampaign = {}; 
-                $scope.campaigns = data;
-              })
-              .error(function(data) {
-                  console.log('Error: ' + data);
-              });
-              $location.path("/manage-campaign");
+            SweetAlert.swal({
+              title: "Sûr de vous ?",
+              text: "Vous êtes sur le point de mettre votre campagne à jour",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#DD6B55",confirmButtonText: "Oui !",
+              cancelButtonText: "Non !",
+              closeOnConfirm: false,
+              closeOnCancel: false },
+
+              function(isConfirm){
+                if (isConfirm) {
+                  $http.put('/api/updateCampaigns/' + $routeParams.campaign_id, $scope.detailCampaign)
+                    .success(function(data) {
+                      $scope.detailCampaign = {};
+                      $scope.campaigns = data;
+                    })
+                    .error(function(data) {
+                        console.log('Error: ' + data);
+                    });
+                    $location.path("/manage-campaign");
+                    SweetAlert.swal("Génial", "Votre campagne a été mise à jour", "success");
+                } else {
+                  SweetAlert.swal("Annulé", "Ouufffff :)", "error");
+                }
+            });
           }
+
 
 
 
